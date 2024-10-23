daa4f553179248267f4f663d7dc5e95e1a5139d0
Szarblewski14 <leonardo27.laurindo@gmail.com>
2024-10-23 19:03:22 -0300

    js feito

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+let btn = document.querySelector('#btn')
 
+function imc () {
+    const nome = document.querySelector('#nome').value
+    const altura = document.querySelector('#altura').value
+    const peso = document.querySelector('#peso').value
+    const resultado = document.querySelector('#resultado')
+    
+    
+    if (nome.value !== '' && altura !== '' && peso !== ''){
+
+        const ValorImc = (peso / (altura * altura)).toFixed(1)
+
+        resultado.textContent = ValorImc
+
+        let classification = ''
+
+        if(ValorImc <= 18.5){
+            classification = 'Abaixo do peso ideal'
+        } else if (ValorImc <= 25) {
+            classification = 'no seu peso ideal'
+        } else {
+            classification = 'acima do peso'
+        }
+
+        resultado.textContent = `${nome}, seu imc é ${ValorImc}, e você está ${classification}`
+
+
+    }else {
+        resultado.textContent = 'preencha todos os campos'
+    }
+}
+
+btn.addEventListener('click', imc)
